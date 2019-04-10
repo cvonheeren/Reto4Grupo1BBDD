d1fa2015a4c90a9741889c1c81cefb058285e4c4
laurimoreni <laura.morenogo@elorrieta-errekamari.com>
2019-04-10 12:35:32 +0200

    migradas las consultas al proyecto de la BBDD

--- src/main/java/Reto4Grupo1BBDD/ModificarBBDD.java
+++ src/main/java/Reto4Grupo1BBDD/ModificarBBDD.java
@@ -1,30 +1,88 @@
 package Reto4Grupo1BBDD;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class ModificarBBDD {
+	
+	Connection conn = null;
+	Pool pool = null;
 
-	/**
-	* Metodo para consultar en la base de datos
-	 * @param con Conexion a la base de datos
-	 * @param query Consulta a realizar de tipo SELECT
-	 * @return ResultSet con los resultados de la consulta realizada
-	 */
-	public ResultSet hacerConsultaBD (Connection con, String query) {
-		ResultSet rs = null;
-		
+	public ModificarBBDD() {
+		pool = new Pool();
+		conectar();
+	}
+	
+	private Connection conectar() {
 		try {
-			Statement st = con.createStatement();
-			rs = st.executeQuery(query);
-		} catch (Exception e) {
+			conn = pool.getConnection();
+		} catch (SQLException e) {
 			//Implementar logger?
 			System.out.println(e.getMessage());
+			JOptionPane.showMessageDialog(new JFrame(), e.getMessage(), "Error en la base de datos",JOptionPane.ERROR_MESSAGE);
+			System.exit(0);
 		}
+		return conn;
+	}
 	
-		return rs;
+	public ArrayList<AlojamientoBBDD> cargarListaAlojamientos(String ciudad) {
+		
+		ArrayList<AlojamientoBBDD> listaAlojamientos = new ArrayList<AlojamientoBBDD>();
+		PreparedStatement stmt = null;
+		ResultSet result = null;
+		String query = "SELECT * FROM HOTEL WHERE NOMBRE LIKE UPPER('%" + ciudad + "%') OR UBICACION LIKE UPPER('%" + ciudad + "%')";
+		
+		try {
+			stmt = conn.prepareStatement(query);
+			result = stmt.executeQuery();
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+		
+		try {
+			while (result.next()) {
+				int cod_alojamiento = result.getInt("COD_HOTEL");
+				String ubicacion = result.getString("UBICACION");
+				String nombre = result.getString("NOMBRE");
+				//float precio = rs.getFloat("PRECIO");
+				listaAlojamientos.add(new AlojamientoBBDD(cod_alojamiento, nombre, ubicacion));
+	        }
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return listaAlojamientos;
+	}
+	
+	public ArrayList<String> cargarListaDestinos() {
+		
+		ArrayList<String> ciudades = new ArrayList<String>();
+		PreparedStatement stmt = null;
+		ResultSet result = null;
+		String query = "SELECT DISTINCT UBICACION FROM HOTEL ORDER BY UBICACION ASC;";
+		
+		try {
+			stmt = conn.prepareStatement(query);
+			result = stmt.executeQuery();
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+		
+		try {
+			while (result.next()) {
+				String ubicacion = result.getString("UBICACION");
+				ciudades.add(ubicacion);
+	        }
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return ciudades;
 	}
 	
 	/**
@@ -33,11 +91,11 @@ public class ModificarBBDD {
 	 * @param query Consulta a realizar de tipo INSERT
 	 * @return boolean true si se ha realizado la insercion con exito, false si no
 	 */
-	public int insertarDatosBD(Connection con, String query) {
+	public int insertarDatosBD(String query) {
 		Statement st;
 		int codReserva;
 		try {
-			st = con.createStatement();
+			st = conn.createStatement();
 		} catch (SQLException e1) {
 			//Implementar logger?
 			e1.printStackTrace();
