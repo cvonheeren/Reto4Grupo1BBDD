9b2ea5b545ce0aa35edaeb00f5a909397b8a0d04
laurimoreni <laura.morenogo@elorrieta-errekamari.com>
2019-05-21 00:03:48 +0200

    filtros servicios

--- src/main/java/Reto4Grupo1BBDD/ModificarBBDD.java
+++ src/main/java/Reto4Grupo1BBDD/ModificarBBDD.java
@@ -65,33 +65,34 @@ public class ModificarBBDD {
 		String orden;
 		String query = null;
 		
+		// servicios
+		int numServicios = servSeleccionados.length;
 		String filtroServicios = "";
-		double sumadorFiltroServ=0;
-		if(!(servSeleccionados.length==0))
-		{
-			filtroServicios = "AND POW(servicios_alojamientos.COD_SERVICIO,4) =";
-				for(int i=0;i<servSeleccionados.length;i++)
-				
-				{
-					System.out.println("COD_SERVICIO: " + servSeleccionados[i] + "Calculo: " + Math.pow(servSeleccionados[i],4));
-					sumadorFiltroServ=sumadorFiltroServ+servSeleccionados[i];
+		if (numServicios > 0) {
+			filtroServicios += "AND COD_SERVICIO IN (";
+			for (int i = 0; i < numServicios;i++) {
+				filtroServicios += "?";
+				if (i != numServicios-1) {
+					filtroServicios += ",";
 				}
-				filtroServicios=filtroServicios+Math.pow(sumadorFiltroServ,4);
+			}
+			if(tipoOrden == 'P') {
+				filtroServicios += ") GROUP BY vistapopularidad.COD_ALOJAMIENTO HAVING COUNT(vistapopularidad.COD_ALOJAMIENTO) = ?";
+			} else if (tipoOrden == 'D') {
+				filtroServicios += ") GROUP BY vistapreciohab.COD_ALOJAMIENTO HAVING COUNT(vistapreciohab.COD_ALOJAMIENTO) = ?";
+			}
+			
 		}
 		
-		
+		// orden
 		if(ordenAscendente)
 			orden="ASC";
 		else
 			orden="DESC";
 		
-		
-		if(tipoOrden == 'P')
-		{
+		if(tipoOrden == 'P') {
 			query = "SELECT * FROM vistapopularidad left join alojamientos on (vistapopularidad.COD_ALOJAMIENTO = alojamientos.COD_ALOJAMIENTO), ubicaciones, servicios_alojamientos WHERE ALOJAMIENTOS.COD_UBICACION = UBICACIONES.COD_UBICACION AND servicios_alojamientos.COD_ALOJAMIENTO=alojamientos.COD_ALOJAMIENTO AND (ALOJAMIENTOS.NOMBRE LIKE UPPER(?) OR UBICACIONES.NOMBRE LIKE UPPER(?) OR UBICACIONES.COD_POSTAL LIKE ?) AND ((ESTRELLAS BETWEEN ? AND ?) OR ESTRELLAS IS NULL) AND TIPO IN (?,?,?) " + filtroServicios + " ORDER BY N_RESERVAS " + orden +";";
-		}
-		else if(tipoOrden == 'D')
-		{
+		} else if(tipoOrden == 'D') {
 			query = "SELECT * FROM vistapreciohab left join alojamientos on (vistapreciohab.COD_ALOJAMIENTO = alojamientos.COD_ALOJAMIENTO), ubicaciones, servicios_alojamientos WHERE ALOJAMIENTOS.COD_UBICACION = UBICACIONES.COD_UBICACION AND servicios_alojamientos.COD_ALOJAMIENTO=alojamientos.COD_ALOJAMIENTO AND (ALOJAMIENTOS.NOMBRE LIKE UPPER(?) OR UBICACIONES.NOMBRE LIKE UPPER(?) OR UBICACIONES.COD_POSTAL LIKE ?) AND ((ESTRELLAS BETWEEN ? AND ?) OR ESTRELLAS IS NULL) AND TIPO IN (?,?,?) " + filtroServicios + " ORDER BY TARIFA_NORMAL " + orden +";";
 		}
 		PreparedStatement stmt = null;
@@ -107,6 +108,13 @@ public class ModificarBBDD {
 			stmt.setString(6, tipoAloj[0]);
 			stmt.setString(7, tipoAloj[1]);
 			stmt.setString(8, tipoAloj[2]);
+			if (numServicios > 0) {
+				int i = 9;
+				for (i = 9; i < numServicios+9;i++) {
+					stmt.setInt(i, servSeleccionados[i-9]);
+				}
+				stmt.setInt(i, numServicios);
+			}
 			result = stmt.executeQuery();
 		} catch (SQLException e1) {
 			e1.printStackTrace();
@@ -210,6 +218,137 @@ public class ModificarBBDD {
 	}
 	
 	/**
+	 * Obtiene los servicios de un alojamiento por su codigo de alojamiento
+	 * @param codAlojamiento
+	 * @return
+	 */
+	public ResultSet obtenerServicios(int codAlojamiento) {
+		PreparedStatement stmt = null;
+		ResultSet result = null;
+		String query = "SELECT S.COD_SERVICIO, S.NOMBRE, SA.PRECIO, FONTAWESOMEICON FROM SERVICIOS S, SERVICIOS_ALOJAMIENTOS SA WHERE S.COD_SERVICIO=SA.COD_SERVICIO AND SA.COD_ALOJAMIENTO=?";
+		try {
+			stmt = conn.prepareStatement(query);
+			stmt.setInt(1, codAlojamiento);
+			result = stmt.executeQuery();
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+			JOptionPane.showMessageDialog(new JFrame(), e1.getMessage(), "Error en la base de datos", JOptionPane.ERROR_MESSAGE);
+			System.exit(0);
+		}
+		return result;
+	}
+	
+	/**
+	 * Obtiene una lista de todos los servicios disponibles
+	 * @return
+	 */
+	public ResultSet obtenerTodosServicios() {
+		PreparedStatement stmt = null;
+		ResultSet result = null;
+		String query = "SELECT * FROM SERVICIOS A;";
+		try {
+			stmt = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+			result = stmt.executeQuery();
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+			JOptionPane.showMessageDialog(new JFrame(), e1.getMessage(), "Error en la base de datos", JOptionPane.ERROR_MESSAGE);
+			System.exit(0);
+		}
+		return result;
+	}
+	
+	/**
+	 * Obtiene los alojamientos que disponen de un servicio
+	 * @param codServicio
+	 * @return
+	 */
+	public ResultSet obtenerAlojporServicio(int codServicio) {
+		PreparedStatement stmt = null;
+		ResultSet result = null;
+		String query = "select cod_alojamiento from servicios_alojamientos where cod_servicio = ?";
+		try {
+			stmt = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+			stmt.setInt(1, codServicio);
+			result = stmt.executeQuery();
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+			JOptionPane.showMessageDialog(new JFrame(), e1.getMessage(), "Error en la base de datos", JOptionPane.ERROR_MESSAGE);
+			System.exit(0);
+		}
+		return result;
+	}
+	
+	/**
+	 * 
+	 * @param codPromo
+	 * @param user
+	 * @param codAlojamiento
+	 * @return
+	 */
+	public ResultSet validarCodPromo(String codPromo, String user, int codAlojamiento) {
+
+		PreparedStatement stmt = null;
+		ResultSet result = null;
+		String query = "SELECT * FROM PROMOCIONES WHERE CODPROMO = ? AND USER_NAME = ? AND COD_ALOJAMIENTO = ?";
+		try {
+			stmt = conn.prepareStatement(query);
+			stmt.setString(1, codPromo);
+			stmt.setString(2, user);
+			stmt.setInt(3, codAlojamiento);
+			result = stmt.executeQuery();
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+			JOptionPane.showMessageDialog(new JFrame(), e1.getMessage(), "Error en la base de datos", JOptionPane.ERROR_MESSAGE);
+			System.exit(0);
+		}
+		return result;
+	}
+
+	/**
+	 * Borra la promicion cuando se ha usado
+	 * @param codPromo
+	 */
+	public void BorrarPromocion(String codPromo) {
+		PreparedStatement stmt = null;
+		String query = "DELETE FROM PROMOCIONES WHERE CODPROMO = ?";
+		
+		try {
+			stmt = conn.prepareStatement(query);
+			stmt.setString(1, codPromo);
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			stmt.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * obtiene el codigo de un cliente segun su user
+	 * @param username
+	 * @return
+	 */
+	public ResultSet obtenerCodCliente(String username) {
+		PreparedStatement stmt = null;
+		ResultSet result = null;
+		String query = "SELECT COD_CLIENTE FROM CLIENTES WHERE USER_NAME = ?";		
+		try {
+			stmt = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+			stmt.setString(1, username);
+			result = stmt.executeQuery();
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+			JOptionPane.showMessageDialog(new JFrame(), e1.getMessage(), "Error en la base de datos", JOptionPane.ERROR_MESSAGE);
+			System.exit(0);
+		}
+		return result;
+	}
+	
+	/**
 	 * Obtiene el cliente correspondiente al dni y contrasena indicados
 	 * @param dniUsuario DNI que se quiere buscar
 	 * @param pass Contrasena que se quiere buscar
@@ -232,6 +371,30 @@ public class ModificarBBDD {
 			System.exit(0);
 		}
 		return result;
+	}
+	
+	/**
+	 * Comprueba que un dni esté o no en la BBDD
+	 * @param dniUsuario El dni que se va a comprobar
+	 * @return El dni si existe
+	 */
+	public ResultSet comprobarDni(String dniUsuario) {
+		PreparedStatement stmt = null;
+		ResultSet result = null;
+		String query = "SELECT * FROM CLIENTES WHERE DNI = ?";
+		Textos textos = new Textos();
+		dniUsuario = textos.encriptar(dniUsuario);
+		try {
+			stmt = conn.prepareStatement(query);
+			stmt.setString(1, dniUsuario);
+			result = stmt.executeQuery();
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+			JOptionPane.showMessageDialog(new JFrame(), e1.getMessage(), "Error en la base de datos", JOptionPane.ERROR_MESSAGE);
+			System.exit(0);
+		}
+		return result;
+	
 	}
 	
 	/**
@@ -326,161 +489,6 @@ public class ModificarBBDD {
 			stmt.setString(7, mail);
 			stmt.executeUpdate();
 			result = stmt.getGeneratedKeys();
-		} catch (SQLException e1) {
-			e1.printStackTrace();
-			JOptionPane.showMessageDialog(new JFrame(), e1.getMessage(), "Error en la base de datos", JOptionPane.ERROR_MESSAGE);
-			System.exit(0);
-		}
-		return result;
-	}
-	
-	/**
-	 * Comprueba que un dni esté o no en la BBDD
-	 * @param dniUsuario El dni que se va a comprobar
-	 * @return El dni si existe
-	 */
-	public ResultSet comprobarDni(String dniUsuario) {
-		PreparedStatement stmt = null;
-		ResultSet result = null;
-		String query = "SELECT * FROM CLIENTES WHERE DNI = ?";
-		Textos textos = new Textos();
-		dniUsuario = textos.encriptar(dniUsuario);
-		try {
-			stmt = conn.prepareStatement(query);
-			stmt.setString(1, dniUsuario);
-			result = stmt.executeQuery();
-		} catch (SQLException e1) {
-			e1.printStackTrace();
-			JOptionPane.showMessageDialog(new JFrame(), e1.getMessage(), "Error en la base de datos", JOptionPane.ERROR_MESSAGE);
-			System.exit(0);
-		}
-		return result;
-	
-	}
-	
-	/**
-	 * 
-	 * @param codPromo
-	 * @param user
-	 * @param codAlojamiento
-	 * @return
-	 */
-	public ResultSet validarCodPromo(String codPromo, String user, int codAlojamiento) {
-
-		PreparedStatement stmt = null;
-		ResultSet result = null;
-		String query = "SELECT * FROM PROMOCIONES WHERE CODPROMO = ? AND USER_NAME = ? AND COD_ALOJAMIENTO = ?";
-		try {
-			stmt = conn.prepareStatement(query);
-			stmt.setString(1, codPromo);
-			stmt.setString(2, user);
-			stmt.setInt(3, codAlojamiento);
-			result = stmt.executeQuery();
-		} catch (SQLException e1) {
-			e1.printStackTrace();
-			JOptionPane.showMessageDialog(new JFrame(), e1.getMessage(), "Error en la base de datos", JOptionPane.ERROR_MESSAGE);
-			System.exit(0);
-		}
-		return result;
-	}
-
-	/**
-	 * Borra la promicion cuando se ha usado
-	 * @param codPromo
-	 */
-	public void BorrarPromocion(String codPromo) {
-		PreparedStatement stmt = null;
-		String query = "DELETE FROM PROMOCIONES WHERE CODPROMO = ?";
-		
-		try {
-			stmt = conn.prepareStatement(query);
-			stmt.setString(1, codPromo);
-		} catch (SQLException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		try {
-			stmt.executeUpdate();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-	/**
-	 * obtiene el codigo de un cliente segun su user
-	 * @param username
-	 * @return
-	 */
-	public ResultSet obtenerCodCliente(String username) {
-		PreparedStatement stmt = null;
-		ResultSet result = null;
-		String query = "SELECT COD_CLIENTE FROM CLIENTES WHERE USER_NAME = ?";		
-		try {
-			stmt = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-			stmt.setString(1, username);
-			result = stmt.executeQuery();
-		} catch (SQLException e1) {
-			e1.printStackTrace();
-			JOptionPane.showMessageDialog(new JFrame(), e1.getMessage(), "Error en la base de datos", JOptionPane.ERROR_MESSAGE);
-			System.exit(0);
-		}
-		return result;
-	}
-	
-	/**
-	 * Obtiene los servicios de un alojamiento por su codigo de alojamiento
-	 * @param codAlojamiento
-	 * @return
-	 */
-	public ResultSet obtenerServicios(int codAlojamiento) {
-		PreparedStatement stmt = null;
-		ResultSet result = null;
-		String query = "SELECT S.COD_SERVICIO, S.NOMBRE, SA.PRECIO, FONTAWESOMEICON FROM SERVICIOS S, SERVICIOS_ALOJAMIENTOS SA WHERE S.COD_SERVICIO=SA.COD_SERVICIO AND SA.COD_ALOJAMIENTO=?";
-		try {
-			stmt = conn.prepareStatement(query);
-			stmt.setInt(1, codAlojamiento);
-			result = stmt.executeQuery();
-		} catch (SQLException e1) {
-			e1.printStackTrace();
-			JOptionPane.showMessageDialog(new JFrame(), e1.getMessage(), "Error en la base de datos", JOptionPane.ERROR_MESSAGE);
-			System.exit(0);
-		}
-		return result;
-	}
-	
-	/**
-	 * Obtiene una lista de todos los servicios disponibles
-	 * @return
-	 */
-	public ResultSet obtenerTodosServicios() {
-		PreparedStatement stmt = null;
-		ResultSet result = null;
-		String query = "SELECT * FROM SERVICIOS A;";
-		try {
-			stmt = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-			result = stmt.executeQuery();
-		} catch (SQLException e1) {
-			e1.printStackTrace();
-			JOptionPane.showMessageDialog(new JFrame(), e1.getMessage(), "Error en la base de datos", JOptionPane.ERROR_MESSAGE);
-			System.exit(0);
-		}
-		return result;
-	}
-	
-	/**
-	 * Obtiene los alojamientos que disponen de un servicio
-	 * @param codServicio
-	 * @return
-	 */
-	public ResultSet obtenerAlojporServicio(int codServicio) {
-		PreparedStatement stmt = null;
-		ResultSet result = null;
-		String query = "select cod_alojamiento from servicios_alojamientos where cod_servicio = ?";
-		try {
-			stmt = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-			stmt.setInt(1, codServicio);
-			result = stmt.executeQuery();
 		} catch (SQLException e1) {
 			e1.printStackTrace();
 			JOptionPane.showMessageDialog(new JFrame(), e1.getMessage(), "Error en la base de datos", JOptionPane.ERROR_MESSAGE);
